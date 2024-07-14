a15d3f5dee068fa48084c410b765899a49b4f45b
minsolkim <123alsthf@naver.com>
2024-07-14 18:47:15 +0900

    init

---
+++ Desktop/fridge/YouthFridge/YouthFridge/YouthFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouthFridgeApp.swift
+//  YouthFridge
+//
+//  Created by 김민솔 on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct YouthFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

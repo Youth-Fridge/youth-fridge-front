5f4a854db6270c3f251dab301df1e764ebc84a7b
minsolkim <123alsthf@naver.com>
2024-07-16 00:51:26 +0900

    mypage list

---
+++ YouthFridge/YouthFridge/View/MyPage/MyActivity/MyActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  MyActivityView.swift
+//  YouthFridge
+//
+//  Created by 김민솔 on 7/16/24.
+//
+
+import SwiftUI
+
+struct MyActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyActivityView()
+}
--- YouthFridge/YouthFridge/View/MyPage/MyPageView.swift
+++ YouthFridge/YouthFridge/View/MyPage/MyPageView.swift
@@ -12,9 +12,10 @@ struct MyPageView: View {
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 20) {
+            VStack(spacing: 0) {
                 welcomeMessage
                 profileView
+                activityList
             }
             .padding()
             .navigationTitle("마이페이지")
@@ -23,35 +24,82 @@ struct MyPageView: View {
     }
     
     var profileView: some View {
-        HStack {
+        HStack() {
             if let user = viewModel.myUser {
                 Image(user.profilePicture)
                     .resizable()
                     .frame(width: 100, height: 100)
                     .clipShape(Circle())
                 VStack(alignment: .leading, spacing: 7) {
-                    Text(user.name)
+                    Text("\(user.name)님")
                         .font(.headline)
+                    Text("초대 모임 예정일")
+                        .font(.subheadline)
+                        .foregroundColor(.main1Color)
+                    Text("07월 30일 화요일 오후 7시")
+                        .font(.caption)
+                        .foregroundColor(.gray6Color)
                 }
+                .padding(.leading,20)
             } else {
                 Text("Loading...")
             }
         }
         .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color.sub1)
     }
     
     var welcomeMessage: some View {
-        if let user = viewModel.myUser {
-            Text("\(user.name) 님\n오늘도 건강한 식사하세요")
-                .font(.title3)
-                .multilineTextAlignment(.leading)
-                .padding()
-        } else {
-            Text("Loading...")
-                .font(.title3)
-                .multilineTextAlignment(.center)
-                .padding()
+        VStack(alignment: .leading, spacing: 8) {
+            if let user = viewModel.myUser {
+                Text("\(user.name)님,")
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+                
+                Text("오늘도 건강한 식사하세요!")
+                    .font(.system(size: 18))
+                    .multilineTextAlignment(.leading)
+            } else {
+                Text("Loading...")
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color.sub1)
+    }
+
+
+    
+    var activityList: some View {
+            List {
+                ForEach(activityItems, id: \.self) { item in
+                    ActivityCell(title: item)
+                }
+            }
+            .listStyle(PlainListStyle())
+        }
+        
+        let activityItems = ["내 활동", "문의", "회원탈퇴"]
+    }
+
+struct ActivityCell: View {
+    var title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.system(size: 18,weight: .bold))
+                .foregroundColor(.black)
+            
+            Spacer()
+            
+            Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+        }
+        .padding(.vertical, 10)
     }
 }
 

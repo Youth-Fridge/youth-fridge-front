4481470dad6783ceec797bcc0d07f8770b9678ed
minsolkim <123alsthf@naver.com>
2024-08-09 00:48:06 +0900

    Feat[#54] 닉네임 userdefaults 저장

--- YouthFridge/YouthFridge/View/Onboarding/ViewModel/ProfileResearchViewModel.swift
+++ YouthFridge/YouthFridge/View/Onboarding/ViewModel/ProfileResearchViewModel.swift
@@ -11,7 +11,11 @@ import CoreLocation
 import Combine
 
 class ProfileResearchViewModel: ObservableObject {
-    @Published var nickname: String = ""
+    @Published var nickname: String = "" {
+            didSet {
+                UserDefaults.standard.set(nickname, forKey: "nickname")
+        }
+    }
     @Published var introduceMe: String = ""
     @Published var isShowingProfileSelector: Bool = false
     @Published var region: MKCoordinateRegion = MKCoordinateRegion()
@@ -32,8 +36,11 @@ class ProfileResearchViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
 
     init() {
-        setupBindings()
-    }
+       if let savedNickname = UserDefaults.standard.string(forKey: "nickname") {
+           self.nickname = savedNickname
+       }
+       setupBindings()
+   }
     
     private func setupBindings() {
         $nickname

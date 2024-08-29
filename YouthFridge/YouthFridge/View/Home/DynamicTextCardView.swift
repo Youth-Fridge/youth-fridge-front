24986b99d8842114698c51c71734614317a9ab8a
minsolkim <123alsthf@naver.com>
2024-08-29 13:04:36 +0900

    Fix[#203] 신청내역 이동

--- YouthFridge/YouthFridge/View/Home/DynamicTextCardView.swift
+++ YouthFridge/YouthFridge/View/Home/DynamicTextCardView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct DynamicTextCardView: View {
     @ObservedObject var viewModel: HomeViewModel
+    @EnvironmentObject var tabSelectionViewModel: TabSelectionViewModel
     
     var body: some View {
         ZStack {
@@ -17,70 +18,89 @@ struct DynamicTextCardView: View {
                 .cornerRadius(10)
             
             if viewModel.daysRemaining == 0 {
-                VStack(alignment: .leading) {
-                    Text("우리 약속의")
-                        .font(.pretendardSemiBold16)
-                        .padding(.leading, 4)
-                    
-                    HStack(spacing: 3) {
-                        ForEach(Array(String("D-DAY")), id: \.self) { char in
-                            Text(String(char))
-                                .font(.pretendardBold32)
-                                .foregroundColor(Color.sub2Color)
-                                .frame(width: 32, height: 38)
-                                .background(Color.white)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray2Color, lineWidth: 1)
-                                )
+                Button(action: {
+                    tabSelectionViewModel.selectedTab = .mypage
+                    tabSelectionViewModel.selectedSubTab = 1
+                    tabSelectionViewModel.shouldNavigateToMyActivity = true
+                }) {
+                    VStack(alignment: .leading) {
+                        Text("우리 약속의")
+                            .font(.pretendardSemiBold16)
+                            .padding(.leading, 4)
+                        
+                        HStack(spacing: 3) {
+                            ForEach(Array(String("D-DAY")), id: \.self) { char in
+                                Text(String(char))
+                                    .font(.pretendardBold32)
+                                    .foregroundColor(Color.sub2Color)
+                                    .frame(width: 32, height: 38)
+                                    .background(Color.white)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray2Color, lineWidth: 1)
+                                    )
+                            }
                         }
                     }
                 }
             } else if viewModel.daysRemaining == -1 {
-                VStack(alignment: .leading) {
-                    Text("우리 약속까지")
-                        .font(.pretendardSemiBold16)
-                    
-                    HStack(spacing: 5) {
-                        ForEach(Array(String(format: "00")), id: \.self) { char in
-                            Text(String(char))
-                                .font(.pretendardBold40)
-                                .foregroundColor(Color.sub2Color)
-                                .frame(width: 40, height: 48)
-                                .background(Color.white)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray2Color, lineWidth: 1)
-                                )
-                        }
-                        Text("일 남았어요")
+                Button(action: {
+                    tabSelectionViewModel.selectedTab = .mypage
+                    tabSelectionViewModel.selectedSubTab = 1
+                    tabSelectionViewModel.shouldNavigateToMyActivity = true
+                }) {
+                    VStack(alignment: .leading) {
+                        Text("우리 약속까지")
                             .font(.pretendardSemiBold16)
-                            .padding(.bottom, -30)
                         
+                        HStack(spacing: 5) {
+                            ForEach(Array(String(format: "00")), id: \.self) { char in
+                                Text(String(char))
+                                    .font(.pretendardBold40)
+                                    .foregroundColor(Color.sub2Color)
+                                    .frame(width: 40, height: 48)
+                                    .background(Color.white)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray2Color, lineWidth: 1)
+                                    )
+                            }
+                            Text("일 남았어요")
+                                .font(.pretendardSemiBold16)
+                                .padding(.bottom, -30)
+                            
+                        }
                     }
                 }
             } else {
-                VStack(alignment: .leading) {
-                    Text("우리 약속까지")
-                        .font(.pretendardSemiBold16)
-                    
-                    HStack(spacing: 5) {
-                        ForEach(Array(String(format: "%02d", viewModel.daysRemaining)), id: \.self) { char in
-                            Text(String(char))
-                                .font(.pretendardBold40)
-                                .foregroundColor(Color.sub2Color)
-                                .frame(width: 40, height: 48)
-                                .background(Color.white)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray2Color, lineWidth: 1)
-                                )
-                        }
-                        Text("일 남았어요")
+                Button(action: {
+                    tabSelectionViewModel.selectedTab = .mypage
+                    tabSelectionViewModel.selectedSubTab = 1
+                    tabSelectionViewModel.shouldNavigateToMyActivity = true
+                }) {
+                    VStack(alignment: .leading) {
+                        Text("우리 약속까지")
                             .font(.pretendardSemiBold16)
-                            .padding(.bottom, -30)
                         
+                        HStack(spacing: 5) {
+                            ForEach(Array(String(format: "%02d", viewModel.daysRemaining)), id: \.self) { char in
+                                Text(String(char))
+                                    .font(.pretendardBold40)
+                                    .foregroundColor(Color.sub2Color)
+                                    .frame(width: 40, height: 48)
+                                    .background(Color.white)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray2Color, lineWidth: 1)
+                                    )
+                            }
+                            Text("일 남았어요")
+                                .font(.pretendardSemiBold16)
+                                .padding(.bottom, -30)
+                            
+                        }
                     }
+                    
                 }
             }
         }
